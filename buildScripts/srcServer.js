c56e52467379ddc3372b030a7a28caf655c3120d
Choi <matthew.choi@accenture.com>
2016-11-23 15:02:10 +1100

    Centralizing HTTP requests

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -24,6 +24,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard cording for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "James", "lastName": "Bond", "email": "james@gmail.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tina@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
